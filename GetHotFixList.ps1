06e0fad3afe9c202c37c8e5a5baaf7d3c4db7ef1
Saif Alhuttaitawi <saifdata@hotmail.com>
2019-11-11 10:48:19 +0100

    Add files via upload

---
+++ GetHotFixList.ps1
@@ -0,0 +1,17 @@
+﻿    #Get Updates List
+    $hotfix= Get-Hotfix
+    
+    # Loop to check if the update was already installed before.
+    foreach ($element in $hotfix.HotFixID)
+        {
+        if ($element -eq "KB4522007")
+           {
+           
+           write-host("The Update in  "+ $computer +" is found!")
+           break
+           }
+           else
+           {
+           write-host("0")
+           }
+        }
---
+++ Remove Files from remote servers.ps1
@@ -0,0 +1,5 @@
+﻿clear
+Invoke-Command -Computer "hsstdb23" -ScriptBlock {Remove-Item -Path C:\2012.ps1 -force -recurse}
+Invoke-Command -Computer "hsstdb23" -ScriptBlock {Remove-Item -Path C:\Explorer-11-for-Windows-Server-2012R2-KB4522007.msu -force -recurse}
+echo "$_ file deleted"
+
